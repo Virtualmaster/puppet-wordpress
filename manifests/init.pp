4930056a49336eb7a3beaa79976ca1d2a6c67cd5
pulecp <pulecp@gmail.com>
2012-12-12 14:45:10 +0100

    fixed redirect in apache

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,6 +19,7 @@ class wordpress(
                 $wordpress_db_server='localhost'
                 $wordpress_app_directory='/opt/wordpress'
                 $wordpress_app_server_name='blog.druidly.com'
+                $wordpress_app_server_alias='blog'
                 )
 {
   $db_name = $wordpress_db_name
@@ -27,6 +28,7 @@ class wordpress(
   $db_server = $wordpress_db_server
   $app_directory = $wordpress_app_directory
   $app_server_name = $wordpress_app_server_name
+  $app_server_alias = $wordpress_app_server_alias
 
   include wordpress::app
   #include wordpress::db
