0e7d9133cec3caada656ee1fb7fb675c05bcd3cd
pulecp <pulecp@gmail.com>
2012-12-12 14:25:03 +0100

    parameterized vhost, location, db commented
    mysql is commented >> not created by puppet
    mysql can be added as ip_address or domain
    
    a can add alias, where wordpress will be running

--- manifests/app.pp
+++ manifests/app.pp
@@ -29,7 +29,7 @@ class wordpress::app {
 
   $vhost_path = $apache ? {
     httpd    => '/etc/httpd/conf.d/wordpress.conf',
-    apache2  => '/etc/apache2/sites-enabled/000-default',
+    apache2  => '/etc/apache2/sites-enabled/000-wordpress',
     default  => '/etc/httpd/conf.d/wordpress.conf',
   }
 
@@ -45,11 +45,11 @@ class wordpress::app {
   file {
     'wordpress_application_dir':
       ensure  =>  directory,
-      path    =>  '/opt/wordpress',
+      path    =>  $app_directory,
       before  =>  File['wordpress_setup_files_dir'];
     'wordpress_setup_files_dir':
       ensure  =>  directory,
-      path    =>  '/opt/wordpress/setup_files',
+      path    =>  "${app_directory}/setup_files",
       before  =>  File[
                       'wordpress_php_configuration',
                       'wordpress_themes',
@@ -59,22 +59,22 @@ class wordpress::app {
                       ];
     'wordpress_installer':
       ensure  =>  file,
-      path    =>  "/opt/wordpress/setup_files/${wordpress_archive}",
+      path    =>  "/${app_directory}/setup_files/${wordpress_archive}",
       notify  =>  Exec['wordpress_extract_installer'],
       source  =>  "puppet:///modules/wordpress/${wordpress_archive}";
     'wordpress_php_configuration':
       ensure     =>  file,
-      path       =>  '/opt/wordpress/wp-config.php',
+      path       =>  "${app_directory}/wp-config.php",
       content    =>  template('wordpress/wp-config.erb'),
       subscribe  =>  Exec['wordpress_extract_installer'];
     'wordpress_htaccess_configuration':
       ensure     =>  file,
-      path       =>  '/opt/wordpress/.htaccess',
+      path       =>  "${app_directory}/.htaccess",
       source     =>  'puppet:///modules/wordpress/.htaccess',
       subscribe  =>  Exec['wordpress_extract_installer'];
     'wordpress_themes':
       ensure     => directory,
-      path       => '/opt/wordpress/setup_files/themes',
+      path       => "${app_directory}/setup_files/themes",
       source     => 'puppet:///modules/wordpress/themes/',
       recurse    => true,
       purge      => true,
@@ -83,7 +83,7 @@ class wordpress::app {
       subscribe  => Exec['wordpress_extract_installer'];
     'wordpress_plugins':
       ensure     => directory,
-      path       => '/opt/wordpress/setup_files/plugins',
+      path       => "${app_directory}/setup_files/plugins",
       source     => 'puppet:///modules/wordpress/plugins/',
       recurse    => true,
       purge      => true,
@@ -93,7 +93,7 @@ class wordpress::app {
     'wordpress_vhost':
       ensure   => file,
       path     => $vhost_path,
-      source   => 'puppet:///modules/wordpress/wordpress.conf',
+      content  => template('wordpress/wordpress_conf.erb'),
       replace  => true,
       require  => Package[$apache];
     }
@@ -101,27 +101,27 @@ class wordpress::app {
       exec {
       'wordpress_extract_installer':
         command      => "unzip -o\
-                        /opt/wordpress/setup_files/${wordpress_archive}\
+                        ${app_directory}/setup_files/${wordpress_archive}\
                         -d /opt/",
         refreshonly  => true,
         require      => Package['unzip'],
         path         => ['/bin','/usr/bin','/usr/sbin','/usr/local/bin'];
       'wordpress_extract_themes':
-        command      => '/bin/sh -c \'for themeindex in `ls \
-                        /opt/wordpress/setup_files/themes/*.zip`; \
+        command      => "/bin/sh -c \'for themeindex in `ls \
+                        ${app_directory}/setup_files/themes/*.zip`; \
                         do unzip -o \
                         $themeindex -d \
-                        /opt/wordpress/wp-content/themes/; done\'',
+                        ${app_directory}/wp-content/themes/; done\'",
         path         => ['/bin','/usr/bin','/usr/sbin','/usr/local/bin'],
         refreshonly  => true,
         require      => Package['unzip'],
         subscribe    => File['wordpress_themes'];
       'wordpress_extract_plugins':
-        command      => '/bin/sh -c \'for pluginindex in `ls \
-                        /opt/wordpress/setup_files/plugins/*.zip`; \
+        command      => "/bin/sh -c \'for pluginindex in `ls \
+                        ${app_directory}/setup_files/plugins/*.zip`; \
                         do unzip -o \
                         $pluginindex -d \
-                        /opt/wordpress/wp-content/plugins/; done\'',
+                        ${app_directory}/wp-content/plugins/; done\'',
         path         => ['/bin','/usr/bin','/usr/sbin','/usr/local/bin'],
         refreshonly  => true,
         require      => Package['unzip'],
--- manifests/init.pp
+++ manifests/init.pp
@@ -15,12 +15,19 @@ import '*.pp'
 class wordpress(
                 $wordpress_db_name='wordpress',
                 $wordpress_db_user='wordpress',
-                $wordpress_db_password='password'
+                $wordpress_db_password='password',
+                $wordpress_db_server='localhost'
+                $wordpress_app_directory='/opt/wordpress'
+                $wordpress_app_server_name='blog.druidly.com'
                 )
 {
   $db_name = $wordpress_db_name
   $db_user = $wordpress_db_user
   $db_password = $wordpress_db_password
+  $db_server = $wordpress_db_server
+  $app_directory = $wordpress_app_directory
+  $app_server_name = $wordpress_app_server_name
+
   include wordpress::app
-  include wordpress::db
+  #include wordpress::db
 }
