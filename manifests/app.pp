8b27860bf1a3b6cdf42eafe14348a00a733af21e
pulecp <pulecp@gmail.com>
2012-12-12 21:54:26 +0100

    newer version of wordpress

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,6 +1,7 @@
 class wordpress::app {
 
-  $wordpress_archive = 'wordpress-3.4.1.zip'
+  #$wordpress_archive = 'wordpress-3.4.1.zip'
+  $wordpress_archive = 'wordpress-3.5.zip'
 
   $apache = $::operatingsystem ? {
     Ubuntu   => apache2,
