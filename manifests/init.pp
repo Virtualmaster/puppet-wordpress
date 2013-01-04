1e8d68c004b00009cca5447d6e3581a01ba3fdd0
Pavel Pulec <pavel.pulec@virtualmaster.com>
2013-01-04 19:27:35 +0100

    new solution - trying

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,20 +13,21 @@
 # [Remember: No empty lines between comments and class definition]
 #import '*.pp'
 class wordpress(
-                $wordpress_db_name='wordpress',
-                $wordpress_db_user='wordpress',
-                $wordpress_db_password='password',
-                $wordpress_db_server='localhost',
-                $wordpress_app_directory='/opt/wordpress',
-                $wordpress_app_server_name='blog1.d.druidly.com',
-                $wordpress_app_server_alias='blog1'
+                $wordpress_db_name = 'wordpress',
+                $wordpress_db_user = 'wordpress',
+                $wordpress_db_password = 'password',
+                $wordpress_db_server = 'localhost',
+                $wordpress_app_directory = '/opt/wordpress',
+                $wordpress_app_server_name = 'blog1.d.druidly.com',
+                $wordpress_app_server_alias = 'blog1'
                 )
 {
   $db_name = $wordpress_db_name
   $db_user = $wordpress_db_user
   $db_password = $wordpress_db_password
   $db_server = $wordpress_db_server
-  $app_directory = $wordpress_app_directory
+# $app_directory = $wordpress_app_directory
+  $app_directory = '/opt/wordpress'
   $app_server_name = $wordpress_app_server_name
   $app_server_alias = $wordpress_app_server_alias
 
