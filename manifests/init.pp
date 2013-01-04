6a7f0e1ba622be1d49bbf5ee86dccd2efc7c6018
Pavel Pulec <pavel.pulec@virtualmaster.com>
2013-01-04 20:50:53 +0100

    ready to use through wordpress::app/db

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,8 +15,8 @@
 class wordpress(
                 $wordpress_db_name = 'wordpress',
                 $wordpress_db_user = 'wordpress',
-                $wordpress_db_password = 'password',
-                $wordpress_db_server = 'localhost',
+                #$wordpress_db_password = 'password',
+                #$wordpress_db_server = 'localhost',
                 $wordpress_app_directory = '/opt/wordpress',
                 $wordpress_app_server_name = 'blog1.d.druidly.com',
                 $wordpress_app_server_alias = 'blog'
